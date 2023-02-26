7d83b7e4cdb196ef4f8c85a7d4327f9940290836
MarcusRattman <zedrattman@gmail.com>
2023-02-26 22:38:17 +0700

    basic swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Animal Farm')
+    .setVersion('1.0')
+    .build();
+
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, doc);
+
   await app.listen(8080);
 }
 bootstrap();
